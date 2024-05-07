5da68913b48883ad4d17bb990b7c4d6dd3f4990b
SilasAlvesTi <silas.ifce.ti@gmail.com>
2024-05-07 08:54:27 -0300

    Adicionando docker

--- Program.cs
+++ Program.cs
@@ -17,6 +17,14 @@ builder.Services.AddScoped<MovieAppearanceService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<PlanetsContext>();
+    dbContext.Database.Migrate();
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
